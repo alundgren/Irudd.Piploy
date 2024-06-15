14c4455388b2de1ba95bea6fa8a56a1fa3b084d0
Irudd <irudd.av@gmail.com>
2024-06-15 04:23:10 +0200

    Refactoring

--- Irudd.Piploy.App/PiployHostBuilder.cs
+++ Irudd.Piploy.App/PiployHostBuilder.cs
@@ -2,17 +2,76 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 
 namespace Irudd.Piploy.App;
 
 internal class PiployHostBuilder
 {
-    public static IHost CreateServiceHost(string[] args) =>    
+    public static RootCommand CreateCommandlineRunner(string[] args, CancellationToken cancellationToken)
+    {
+        async Task Status(InvocationContext context)
+        {
+            var host = CreateConfigOnlyHost(args);
+            var service = host.Services.GetRequiredService<PiployService>();
+            var statusText = await service.GetStatusText(cancellationToken);
+            Console.WriteLine(statusText);
+        }
+
+        async Task StartService(InvocationContext context)
+        {
+            var host = CreateServiceHost(args);
+            await host.RunAsync();
+        }
+
+        async Task StopService(InvocationContext context)
+        {
+            await PiployBackgroundService.SendCommand("stop", cancellationToken);
+        }
+
+        async Task Poll(InvocationContext context)
+        {
+            if (await PiployBackgroundService.IsBackgroundServiceRunning(cancellationToken))
+            {
+                Console.WriteLine("Background service running. Sending the poll command there");
+                await PiployBackgroundService.SendCommand("poll", cancellationToken);
+            }
+            else
+            {
+                var host = CreateConfigOnlyHost(args);
+                var service = host.Services.GetRequiredService<PiployService>();
+                await service.Poll(cancellationToken);
+            }
+        }
+
+        async Task WipeAll(InvocationContext context)
+        {
+            var host = CreateConfigOnlyHost(args);
+            var service = host.Services.GetRequiredService<PiployService>();
+            await service.WipeAll(cancellationToken);
+        }
+
+        var rootCommand = new RootCommand("piploy raspberry pi + git + docker host");
+        rootCommand.SetHandler(Status);
+
+        void AddCommand(string name, string description, Func<InvocationContext, Task> handle)
+        {
+            var command = new Command(name, description);
+            command.SetHandler(handle);
+            rootCommand.Add(command);
+        }
+
+        AddCommand("status", "Service status", Status);
+        AddCommand("service-start", "Run as service", StartService);
+        AddCommand("service-stop", "Run as service", StopService);
+        AddCommand("poll", "Poll for changes now", Poll);
+        AddCommand("wipeall", "Wipes out all local repos, docker images and containers", WipeAll);
+
+        return rootCommand;
+    }
+
+    private static IHost CreateServiceHost(string[] args) =>
         CreateBuilderWithConfigOnly(args)
         .ConfigureServices((hostContext, services) =>
         {
@@ -20,7 +79,8 @@ internal class PiployHostBuilder
         })
         .Build();
 
-    public static IHost CreateConfigOnlyHost(string[] args) => CreateBuilderWithConfigOnly(args).Build();
+    private static IHost CreateConfigOnlyHost(string[] args) => CreateBuilderWithConfigOnly(args).Build();
+
 
     private static IHostBuilder CreateBuilderWithConfigOnly(string[] args) => Host
         .CreateDefaultBuilder(args)
--- Irudd.Piploy.App/Program.cs
+++ Irudd.Piploy.App/Program.cs
@@ -1,66 +1,8 @@
 ﻿using Irudd.Piploy.App;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System.CommandLine;
-using System.CommandLine.Invocation;
 
 using CancellationTokenSource tokenSource = new CancellationTokenSource();
 
-async Task Status(InvocationContext context)
-{
-    var host = PiployHostBuilder.CreateConfigOnlyHost(args);
-    var service = host.Services.GetRequiredService<PiployService>();
-    var statusText = await service.GetStatusText(tokenSource.Token);
-    Console.WriteLine(statusText);
-}
-
-async Task StartService(InvocationContext context)
-{
-    var host = PiployHostBuilder.CreateServiceHost(args);
-    await host.RunAsync();
-}
-
-async Task StopService(InvocationContext context)
-{
-    await PiployBackgroundService.SendCommand("stop", tokenSource.Token);
-}
-
-async Task Poll(InvocationContext context)
-{    
-    if(await PiployBackgroundService.IsBackgroundServiceRunning(tokenSource.Token))
-    {
-        Console.WriteLine("Background service running. Sending the poll command there");
-        await PiployBackgroundService.SendCommand("poll", tokenSource.Token);
-    }
-    else
-    {
-        var host = PiployHostBuilder.CreateConfigOnlyHost(args);
-        var service = host.Services.GetRequiredService<PiployService>();
-        await service.Poll(tokenSource.Token);
-    }
-}
-
-async Task WipeAll(InvocationContext context)
-{
-    var host = PiployHostBuilder.CreateConfigOnlyHost(args);
-    var service = host.Services.GetRequiredService<PiployService>();
-    await service.WipeAll(tokenSource.Token);
-}
-
-var rootCommand = new RootCommand("piploy raspberry pi + git + docker host");
-rootCommand.SetHandler(Status);
-
-void AddCommand(string name, string description, Func<InvocationContext, Task> handle)
-{
-    var command = new Command(name, description);
-    command.SetHandler(handle);
-    rootCommand.Add(command);
-}
-
-AddCommand("status", "Service status", Status);
-AddCommand("service-start", "Run as service", StartService);
-AddCommand("service-stop", "Run as service", StopService);
-AddCommand("poll", "Poll for changes now", Poll);
-AddCommand("wipeall", "Wipes out all local repos, docker images and containers", WipeAll);
+var rootCommand = PiployHostBuilder.CreateCommandlineRunner(args, tokenSource.Token);
 
 await rootCommand.InvokeAsync(args);
